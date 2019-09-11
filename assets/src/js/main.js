846afc7b3abb7402b748452a2a28240019706789
Mario Aguiar <me@marioaguiar.net>
2019-09-11 10:59:34 -0500

    Remove tooltip in favor of a different approach

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,17 +1,9 @@
 /* eslint-disable no-unused-vars */
-import tooltip from '@10up/component-tooltip';
 import Bio from './components/bio';
 
 ( function() {
 	// eslint-disable-next-line no-console
 	console.log( 'A foo walks into a bar, takes a look around and says "Hello World!"' );
-
-	const tooltipSelector = '.stories';
-
-	const stories = new tooltip( tooltipSelector, () => {
-		// eslint-disable-next-line no-console
-		console.log( 'tooltip init' );
-	} );
 
 	const bio = new Bio( '#bio' );
 
