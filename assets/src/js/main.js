478e8d1c7286af0d4c93f5c8ac422f2cd34ffa3d
Mario Aguiar <me@marioaguiar.net>
2019-09-11 11:00:08 -0500

    Remove console statement

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -2,9 +2,6 @@
 import Bio from './components/bio';
 
 ( function() {
-	// eslint-disable-next-line no-console
-	console.log( 'A foo walks into a bar, takes a look around and says "Hello World!"' );
-
 	const bio = new Bio( '#bio' );
 
 	bio.collapse();
